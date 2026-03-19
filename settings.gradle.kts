6a0f974e2538c14d8a7f657c22371ebeed8c833c
Дмитрий Красилов <dfkrasilov@gmail.com>
2026-03-19 10:52:06 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,1 @@
-pluginManagement {
-    repositories {
-        maven(url = "https://nexus.sberdevices.ru/repository/gradle-proxy/")
-    }
-}
-dependencyResolutionManagement {
-    repositories {
-        maven(url = "https://nexus.sberdevices.ru/repository/maven-central/")
-    }
-}
-
 rootProject.name = "ihub-runner-spy"
